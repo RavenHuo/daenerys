4a292e8643f0b052c05e9633ea7ef47811532bbd
huoruiwen <ruiwen.rw@nonolive.com>
2022-07-22 16:43:32 +0800

    fix server options

--- http/server/flow.go
+++
@@ -1,6 +0,0 @@
-/**
- * @Author raven
- * @Description
- * @Date 2022/7/7
- **/
-package server
--- http/server/intercept.go
+++ http/server/intercept.go
@@ -6,8 +6,6 @@
 package server
 
 import (
-	"errors"
-	"fmt"
 	"sort"
 
 	"github.com/RavenHuo/daenerys/core"
@@ -26,54 +24,4 @@ func sortHandlerIntercept(intercepts []HandlerIntercept) {
 	sort.Slice(intercepts, func(i, j int) bool {
 		return intercepts[i].Order() > intercepts[j].Order()
 	})
-}
-
-// 过滤器
-type HandlerFilter interface {
-	core.Order
-	DoFilter(*Context, *HandlerFilterChain) error
-}
-
-func sortHandlerFilter(filters []HandlerFilter) {
-	sort.Slice(filters, func(i, j int) bool {
-		return filters[i].Order() > filters[j].Order()
-	})
-}
-
-func MakeFilterChain(filters []HandlerFilter) *HandlerFilterChain {
-	sortHandlerFilter(filters)
-	return &HandlerFilterChain{
-		pos:     -1,
-		filters: filters,
-	}
-}
-
-type HandlerFilterChain struct {
-	/**
-	 * The int which is used to maintain the current position
-	 * in the filter requestNode.
-	 */
-	pos     int
-	filters []HandlerFilter
-}
-
-func (chain *HandlerFilterChain) DoFilter(ctx *Context) error {
-	if len(chain.filters) == 0 || chain.pos == len(chain.filters)-1 {
-		return nil
-	}
-	chain.pos += 1
-	err := chain.internalDoFilter(ctx)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (chain *HandlerFilterChain) internalDoFilter(ctx *Context) error {
-	filter := chain.filters[chain.pos]
-	err := filter.DoFilter(ctx, chain)
-	if err != nil {
-		return errors.New(fmt.Sprintf("internalDoFilter:%s filter err %s", filter.Name(), err))
-	}
-	return nil
 }
